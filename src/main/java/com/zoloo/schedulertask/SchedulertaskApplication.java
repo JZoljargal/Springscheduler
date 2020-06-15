7636a298772124f3f36b97ede8fdc8738bfd41af
JZoljargal <zoloo2010lucky@gmail.com>
2020-06-15 21:46:56 +0800

    added

---
+++ src/main/java/com/zoloo/schedulertask/SchedulertaskApplication.java
@@ -0,0 +1,13 @@
+package com.zoloo.schedulertask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulertaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulertaskApplication.class, args);
+	}
+
+}
